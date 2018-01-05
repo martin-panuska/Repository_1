779770c76244078c61c288053fba348ecabed85f
Mato <Mato@DESKTOP-VP81O50>
2018-01-05 22:18:27 +0100

    Initial share

---
+++ RestWebProject/src/com/martin/web/rest/service/HelloRest.java
@@ -0,0 +1,29 @@
+package com.martin.web.rest.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/helloRest")
+public class HelloRest {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello Zuzka";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+	}
+
+	// This method is called if HTML is request
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello Jersey" + "</title>" + "<body><h1>" + "Hello Jersey" + "</body></h1>"
+				+ "</html> ";
+	}
+}
