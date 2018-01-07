5e1d16d5caee95e5ea9af5d14fcaafd16006d6ba
Mato <Mato@DESKTOP-VP81O50>
2018-01-07 10:24:37 +0100

    Initial share

---
+++ RestCommon/src/com/martin/model/Address.java
@@ -0,0 +1,47 @@
+package com.martin.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Address {
+	private String state;
+	private String city;
+	private String street;
+	private String zip;
+	
+	public Address() {
+		super();
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+}
---
+++ RestCommon/src/com/martin/model/Person.java
@@ -0,0 +1,49 @@
+package com.martin.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.martin.model.constant.Gender;
+
+@XmlRootElement
+public class Person {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private Gender gender;
+	
+	public Person() {
+		super();
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+}
